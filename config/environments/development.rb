d5b0dd6d804957ca7825494c5215e44d1bdf1d66
Adam Hanna <adam.s.hanna@gmail.com>
2018-12-22 00:35:59 +0000

    fix typo with maigun config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-config.action_mailer.default_url_options = { host: '184.173.153.207 ', port: '587' }
+config.action_mailer.default_url_options = { host: '184.173.153.207', port: 587 }
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
@@ -43,7 +43,7 @@ config.action_mailer.default_url_options = { host: '184.173.153.207 ', port: '58
     :port => 587,
     :domain => "sandbox03853a1698174e70ab7d8cdbcea6d374.mailgun.org",
     :user_name => "postmaster@sandbox03853a1698174e70ab7d8cdbcea6d374.mailgun.org",
-    :password => "104e987ffd0ed89d5bafcb92983679ec-9b463597-82bbae4"
+    :password => "sambarama!123"
   }
 
   # Print deprecation notices to the Rails logger.
