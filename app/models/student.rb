a7ff65099790d0d146f9b06d2958469c61e7f587
Adam Hanna <adam.s.hanna@gmail.com>
2018-12-30 03:08:36 +0000

    add lessons remaining view in students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ApplicationRecord
+    has_many :packages
+    
+    def lessons_remaining
+        @lessons = 0
+        self.packages.each do |p|
+            @lessons += p.remaining
+        end
+        return @lessons
+    end
 end
