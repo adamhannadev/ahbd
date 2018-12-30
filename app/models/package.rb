4c40bb3eeb5b8d3216a71c336404f7b1567a3db3
Adam Hanna <adam.s.hanna@gmail.com>
2018-12-30 02:34:46 +0000

    fix error with package activity

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,15 +2,25 @@ class Package < ApplicationRecord
   belongs_to :student
   belongs_to :payment
   has_many :lessons, dependent: :destroy
+  after_initialize :set_activity
   
-  def active
-   if self.lessons.count < self.lessons_purchased
-   return true
-   end
+  def remaining
+   return self.lessons_purchased - self.lessons.count
   end
+  
+  
   
   def title
    self.student.last_name + ' - ' + self.payment.payment_date.strftime("%m/%d/%Y")
   end
+
+ private
+ def set_activity
+  if self.lessons.count == self.lessons_purchased
+    self.active = false
+  else
+    self.active = true
+  end
+ end
 
 end
