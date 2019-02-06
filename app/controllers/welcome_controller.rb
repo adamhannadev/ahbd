f7789a193c1cd487556245a7fab583a51c2031ca
Adam Hanna <adam.s.hanna@gmail.com>
2019-02-06 20:00:45 +0000

    create excel backups for lessons, payments and packages.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,17 @@
 class WelcomeController < ApplicationController
     
+    def backup
+        @lessons = Lesson.all
+        @packages = Package.all
+        @payments = Payment.all
+         respond_to do |format|
+    format.xlsx {
+    response.headers['Content-Disposition'] = 'attachment; filename="all_backup.xlsx"'
+    }
+    end 
+    end
+    
     def index
-        
+    
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   resources :students
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "backup", to: "welcome#backup"
   root 'welcome#index'
 end
