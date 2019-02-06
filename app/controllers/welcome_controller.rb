61d78ca665fa6d8043694c3c1c92f7e53d7e5585
Adam Hanna <adam.s.hanna@gmail.com>
2019-02-06 20:02:58 +0000

    add authentication for backups

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-    
+    before_action :authenticate_user!, :ensure_admin!, only:[:backup]
     def backup
         @lessons = Lesson.all
         @packages = Package.all
